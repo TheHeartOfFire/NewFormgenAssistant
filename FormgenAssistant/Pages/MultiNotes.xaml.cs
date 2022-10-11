d101c930ee328a26d776ce0abb7200ef00dfa5f5
Dakota Jordan <heartoffiremedia@yahoo.com>
2022-10-11 16:58:26 -0500

    style testing

--- FormgenAssistant/Pages/MultiNotes.xaml.cs
+++ FormgenAssistant/Pages/MultiNotes.xaml.cs
@@ -22,40 +22,45 @@ public partial class MultiNotes : UserControl
     public MultiNotes()
     {
         InitializeComponent();
+        CreateTab("Item1");
+        CreateTab("Item2");
+        CreateTab("Item3");
     }
-    //https://stackoverflow.com/questions/62463491/how-to-make-a-quick-window-to-rename-tab
-    //public partial class TabEditWindow : Window
-    //{
-    //    public TabEditWindow(TabControl tabs)
-    //    {
-    //        this.InitializeComponent();
-    //        this.tabs = tabs;
-    //        base.Topmost = true;
-    //    }
+    
+    public void CreateTab(string Name)
+    {
+        //https://social.msdn.microsoft.com/Forums/vstudio/en-US/035f7878-c577-486d-8561-3d193bb21fbe/ho-to-allow-the-user-to-edit-the-tab-header-?forum=wpf
+        // Create DataTemplate
+        DataTemplate tabItemTemplate = new DataTemplate();
+        tabItemTemplate.DataType = typeof(TabItem);
 
+        // Create TextBox
+        FrameworkElementFactory textBoxFactory = new FrameworkElementFactory(typeof(TextBox));
+        textBoxFactory.SetBinding(TextBox.TextProperty, new Binding("."));
+        textBoxFactory.SetValue(TextBox.BackgroundProperty, new SolidColorBrush(Color.FromArgb(255,15,11,30)));
+        var fore = System.Drawing.Color.MediumSlateBlue;
+        textBoxFactory.SetValue(TextBox.ForegroundProperty, new SolidColorBrush(Color.FromArgb(fore.A, fore.R, fore.G, fore.B)));
+        textBoxFactory.SetValue(TextBox.BorderBrushProperty, new SolidColorBrush(Color.FromArgb(255, 15, 11, 30)));
 
-    //    public void Show(TabItem tab)
-    //    {
-    //        this.tab = tab;
-    //        base.Show();
-    //    }
+        var focusedStyle = new Style(typeof(TextBox));
+        var focusedBorder = new Setter();
+        focusedBorder.Property = TextBox.BorderBrushProperty;
+        focusedBorder.Value = new SolidColorBrush(Color.FromArgb(255, 15, 11, 30));
+        focusedStyle.Setters.Add(focusedBorder);
 
-    //    private void RenameTab(object sender, RoutedEventArgs e)
-    //    {
-    //        base.Hide();
-    //        TextBox textBox = this.tab.Header as TextBox;
-    //        textBox.IsEnabled = true;
-    //        textBox.Focus();
-    //        textBox.SelectAll();
-    //    }
+        textBoxFactory.SetValue(TextBox.FocusVisualStyleProperty, focusedStyle);
+        
+        tabItemTemplate.VisualTree = textBoxFactory;
 
-    //    private void CloseTab(object sender, RoutedEventArgs e)
-    //    {
-    //        base.Hide();
-    //        this.tabs.Items.Remove(this.tab);
-    //    }
+        // Create the TabItem
+        TabItem item1 = new TabItem();
+        item1.HeaderTemplate = tabItemTemplate;
+        item1.Header = Name;
+        item1.Content = new Notes();
+        item1.BorderBrush = new SolidColorBrush(Color.FromArgb(fore.A, fore.R, fore.G, fore.B));
+        item1.Background = new SolidColorBrush(Color.FromArgb(255, 15, 11, 30));
 
-    //    private TabControl tabs;
-    //    private TabItem tab;
-    //}
+        // Add the TabItem to the TabControl
+        tcContent.Items.Add(item1);
+    }
 }
